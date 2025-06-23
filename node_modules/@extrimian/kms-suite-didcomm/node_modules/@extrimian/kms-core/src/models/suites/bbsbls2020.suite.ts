f736a379443ca47b69928bb943fe7aa8a0e30016
Jake Jones <jake.jones@bsvblockchain.org>
2025-06-22 22:56:11 -0600

    fix: fixing dependencies

--- node_modules/@extrimian/kms-suite-didcomm/node_modules/@extrimian/kms-core/src/models/suites/bbsbls2020.suite.ts
+++
@@ -1,8 +0,0 @@
-import { IKeyPair } from "../keypair";
-import { Purpose } from "@extrimian/did-core";
-import { IVCJsonLDKeyPair, IVCSuite } from "./vc.suite";
-
-export interface IBbsBls2020Suite extends IVCSuite {
-    create: () => Promise<IVCJsonLDKeyPair>;
-    sign: (documentToSign: string, did: string, verificationMethodId: string, porpuse: Purpose) => Promise<any>;
-}
